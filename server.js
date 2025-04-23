da86283b9e4902774fae6d5f3f33c2200029eedd
john kassahC:\Users\johnk\Desktop\Web <johnkassahwebdev@gmail.com>
2025-04-23 20:28:57 +0000

    same

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 // Eneble cross-origin resource sharing (CORS) in your app. This will allow your app to accept requests from other domains and ports
 // and allow your app to send requests to other domains and ports. This is important for security reasons.
 app.use(cors({
-    origin: 'http://192.168.43.130:5173', // Your frontend's URL
+    origin: 'http://192.168.43.130:5173/', // Your frontend's URL
     credentials: true
 }));
 
